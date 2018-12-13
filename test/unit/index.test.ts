9b2ff6e9c072ef605a604bec129d11d07e4d4f78
WMXPY <wm@mengw.io>
2018-12-12 18:10:58 -0600

    init

---
+++ test/unit/index.test.ts
@@ -0,0 +1,16 @@
+/**
+ * @author WMXPY
+ * @namespace Extract
+ * @description Index
+ * @package Unit Test
+ */
+
+import { expect } from 'chai';
+
+describe('Given {SafeExtract} Class', (): void => {
+
+    it('p', (): void => {
+
+        expect(1).to.be.equal(1);
+    });
+});
