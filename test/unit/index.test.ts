2ff4ad297ebd16b8a7045816dc5f356cebd7df43
PCXPY <pc@wmpcxpy.com>
2018-12-13 15:30:41 -0600

    [Test] finish up unit tests

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -42,4 +42,51 @@ describe('Given {SafeExtract} Class', (): void => {
         const actual: string = instance.safe('a').value;
         expect(actual).to.be.equal(value);
     });
+
+    it('should throw an error when property is not exist', (): void => {
+
+        const instance: SafeExtract<{
+            a: string;
+        }> = SafeExtract.create({});
+
+        const expr = () => instance.safe('a').value;
+        expect(expr).to.be.throw('[Sudoo-Extract] Extract failed');
+    });
+
+    it('should be able to custom error', (): void => {
+
+        const errorMessage: string = chance.string();
+        const instance: SafeExtract<{
+            a: string;
+        }> = SafeExtract.create<{
+            a: string;
+        }>({}).onError(new Error(errorMessage));
+
+        const expr = () => instance.safe('a').value;
+        expect(expr).to.be.throw(errorMessage);
+    });
+
+    it('should be able to get unsafe value', (): void => {
+
+        const value: string = chance.string();
+        const instance: SafeExtract<{
+            a: string;
+        }> = SafeExtract.create({
+            a: value,
+        });
+
+        const actual: SafeExtract<string> = instance.unsafe('a') as SafeExtract<string>;
+        expect(actual.value).to.be.equal(value);
+    });
+
+    it('should be able to get null when unsafe value not exist', (): void => {
+
+        const instance: SafeExtract<{
+            a: string;
+        }> = SafeExtract.create({});
+
+        const actual: SafeExtract<string> | null = instance.unsafe('a');
+        // tslint:disable-next-line
+        expect(actual).to.be.null;
+    });
 });
